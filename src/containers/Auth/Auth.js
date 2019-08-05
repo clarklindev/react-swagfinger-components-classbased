361bb37c04b33146d3ef02bc6ad6a340c999b62b
Clark Lin <clarklindeveloper@gmail.com>
2019-08-05 14:30:21 +0200

    hook up validation classes

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-import classes from './Auth.module.scss';
-import Utils from '../../Utils';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
-
+import classes from './Auth.module.scss';
 class Auth extends Component {
 	state = {
 		controls: {
@@ -38,13 +36,54 @@ class Auth extends Component {
 		}
 	};
 
-	render() {
-		let className = Utils.getClassNameString([
-			classes.Auth,
-			Auth.name,
-			this.props.className
-		]);
+	checkValidity(value, rules) {
+		let isValid = true;
+		if (!rules) {
+			return true;
+		}
 
+		if (rules.required) {
+			isValid = value.trim() !== '' && isValid;
+		}
+
+		if (rules.minLength) {
+			isValid = value.length >= rules.minLength && isValid;
+		}
+
+		if (rules.maxLength) {
+			isValid = value.length <= rules.maxLength && isValid;
+		}
+
+		if (rules.isEmail) {
+			const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+			isValid = pattern.test(value) && isValid;
+		}
+
+		if (rules.isNumeric) {
+			const pattern = /^\d+$/;
+			isValid = pattern.test(value) && isValid;
+		}
+
+		return isValid;
+	}
+
+	inputChangedHandler = (event, controlName) => {
+		const updatedControls = {
+			...this.state.controls,
+			[controlName]: {
+				...this.state.controls[controlName],
+				value: event.target.value,
+				valid: this.checkValidity(
+					event.target.value,
+					this.state.controls[controlName].validation
+				),
+				touched: true
+			}
+		};
+		this.setState({ controls: updatedControls });
+	};
+
+	render() {
 		const formElementsArray = [];
 		for (let key in this.state.controls) {
 			formElementsArray.push({
@@ -66,7 +105,7 @@ class Auth extends Component {
 		));
 
 		return (
-			<div className={className}>
+			<div className={classes.Auth}>
 				<form>
 					{form}
 					<Button btnType="Success">Submit</Button>
