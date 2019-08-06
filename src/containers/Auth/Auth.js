dbff59059a823685c173ebf52dece76ef68ecd59
Clark Lin <clarklindeveloper@gmail.com>
2019-08-06 15:48:04 +0200

    authentication -  Adding Sign-In

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -35,7 +35,8 @@ class Auth extends Component {
 				valid: false,
 				touched: false
 			}
-		}
+		},
+		isSignUp: true
 	};
 
 	checkValidity(value, rules) {
@@ -89,8 +90,15 @@ class Auth extends Component {
 		event.preventDefault(); //prevents reloading of page and event bubbling
 		this.props.onAuth(
 			this.state.controls.email.value,
-			this.state.controls.password.value
+			this.state.controls.password.value,
+			this.state.isSignUp
 		);
+	};
+
+	switchAuthModeHandler = () => {
+		this.setState(prevState => {
+			return { isSignUp: !prevState.isSignUp };
+		});
 	};
 
 	render() {
@@ -120,6 +128,9 @@ class Auth extends Component {
 					{form}
 					<Button btnType="Success">Submit</Button>
 				</form>
+				<Button btnType="Danger" onClick={this.switchAuthModeHandler}>
+					Switch to {this.state.isSignUp ? 'SIGN-IN' : 'SIGN-UP'}
+				</Button>
 			</div>
 		);
 	}
@@ -127,7 +138,8 @@ class Auth extends Component {
 
 const mapDispatchToProps = dispatch => {
 	return {
-		onAuth: (email, password) => dispatch(actions.auth(email, password))
+		onAuth: (email, password, isSignup) =>
+			dispatch(actions.auth(email, password, isSignup))
 	};
 };
 export default connect(
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,6 @@
 import * as actionTypes from './actionsTypes';
+import axios from 'axios';
+
 export const authStart = () => {
 	return {
 		type: actionTypes.AUTH_START
@@ -19,8 +21,31 @@ export const authFail = error => {
 };
 
 //async
-export const auth = (email, password) => {
+export const auth = (email, password, isSignUp) => {
 	return dispatch => {
 		dispatch(authStart());
+		const authData = {
+			email: email,
+			password: password,
+			returnSecureToken: true
+		};
+
+		let url =
+			'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAJzYSyjWZ0D45Prfs_8h1BjuL_PWh_bOY';
+		if (!isSignUp) {
+			url =
+				// note firebase v3 is diff from v1, v3: /identitytoolkit/v3/relyingparty/verifyPassword?key=[API_KEY]
+				'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAJzYSyjWZ0D45Prfs_8h1BjuL_PWh_bOY';
+		}
+		axios
+			.post(url, authData)
+			.then(response => {
+				console.log(response);
+				dispatch(authSuccess(response.data));
+			})
+			.catch(err => {
+				console.log(err);
+				dispatch(authFail(err));
+			});
 	};
 };
