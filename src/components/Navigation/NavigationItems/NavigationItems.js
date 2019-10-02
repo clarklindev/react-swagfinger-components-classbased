78b7634412b934bd803ee8336198fcbb110205a8
Clark <clarklindeveloper@gmail.com>
2019-10-02 14:34:53 +0200

    remove authentication from nav

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,7 +5,7 @@ import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = () => (
   <ul className={classes.NavigationItems}>
-    <NavigationItem link="/auth">Auth</NavigationItem>
+    {/* <NavigationItem link="/auth">Auth</NavigationItem> */}
     <NavigationItem link="/phonebook">Phonebook</NavigationItem>
     <NavigationItem link="/phonebookadmin">Phonebook Admin</NavigationItem>
   </ul>
