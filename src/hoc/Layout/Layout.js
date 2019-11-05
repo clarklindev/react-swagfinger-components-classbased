e262e311cbb942b75d8c70a920f855aa9f8cb0c5
Clark <clarklindeveloper@gmail.com>
2019-11-05 10:00:09 +0200

    remove Aux class for React.Fragment

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 
-import Aux from '../Auxiliary/Auxiliary';
 import classes from './Layout.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 
 class Layout extends Component {
-	render() {
-		return (
-			<Aux>
-				<Toolbar />
-				<main className={classes.Content}>{this.props.children}</main>
-			</Aux>
-		);
-	}
+  render() {
+    return (
+      <React.Fragment>
+        <Toolbar />
+        <main className={classes.Layout}>{this.props.children}</main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Layout;
