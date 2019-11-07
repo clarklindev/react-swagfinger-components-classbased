baa2a4e85f14779e99e87cfd797ffa33cb5d6b78
Clark <clarklindeveloper@gmail.com>
2019-11-07 15:56:38 +0200

    update indent spacing to 2

--- src/store/utility.js
+++ src/store/utility.js
@@ -1,5 +1,5 @@
 export const updateObject = (oldObject, updatedValues) => {
-	let newObject = { ...oldObject, ...updatedValues };
-	console.log('after update: ', newObject);
-	return newObject;
+  let newObject = { ...oldObject, ...updatedValues };
+  console.log('after update: ', newObject);
+  return newObject;
 };
