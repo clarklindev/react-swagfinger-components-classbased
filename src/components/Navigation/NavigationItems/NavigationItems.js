8a64f1653160296dfeac02148e8ebc4cb42c901a
Clark <clarklindeveloper@gmail.com>
2019-11-14 22:59:15 +0200

    switched login button position on navbar

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import classes from './NavigationItems.module.scss';
-import NavigationItem from './NavigationItem/NavigationItem';
+import classes from "./NavigationItems.module.scss";
+import NavigationItem from "./NavigationItem/NavigationItem";
 
 const navigationItems = () => (
   <ul className={classes.NavigationItems}>
     {/* <NavigationItem link="/auth">Auth</NavigationItem> */}
-    <NavigationItem link="/login">Login</NavigationItem>
     <NavigationItem link="/phonebook">Phonebook</NavigationItem>
     <NavigationItem link="/phonebookadmin">Phonebook Admin</NavigationItem>
+    <NavigationItem link="/login">Login</NavigationItem>
   </ul>
 );
 
