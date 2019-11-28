fc00eb0d803d7f9ef34152c3668bc6bb71cda43d
Clark <clarklindeveloper@gmail.com>
2019-11-28 21:09:33 +0200

    Fixed SelectWithInput height, checkbox cursor

--- src/components/UI/InputComponents/Checkbox.js
+++ src/components/UI/InputComponents/Checkbox.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import classes from './Checkbox.module.scss';
 
 class Checkbox extends Component {
   render() {
     return (
-      <div className='checkbox'>
+      <div className={classes.Checkbox}>
         <input type='checkbox' id={'checkbox' + this.props.name} />
         <label htmlFor={'checkbox' + this.props.name}>{this.props.label}</label>
       </div>
