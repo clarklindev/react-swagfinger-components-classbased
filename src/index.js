449c2d853dd711acd0743a174665ad65413bd339
Clark <clarklindeveloper@gmail.com>
2020-01-21 07:44:40 +0200

    fix error for REDUX devtools not available on Edge Browser

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ const rootReducer = combineReducers({
 });
 const composeEnhancers =
   process.env.NODE_ENV === 'development'
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
     : null || compose;
 
 const store = createStore(
