e25610920730d6f02f29a272a3ba9d66d0f652ce
Clark <clarklindeveloper@gmail.com>
2020-01-22 21:22:48 +0200

    removed Default borders from List and added to ListItem, all have their own borders, List map() safety check || []

--- src/components/UI/InputComponents/List.js
+++ src/components/UI/InputComponents/List.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import classes from './List.module.scss';
+import Utils from '../../../Utils';
 
 class List extends Component {
   render() {
+    let classList = Utils.getClassNameString([
+      classes.List,
+      List.name,
+      this.props.className
+    ]);
     return (
-      <div className={classes.List}>
+      <div className={classList}>
         <ul>
-          {this.props.value.data.map((item, index) => {
+          {(this.props.value.data || []).map((item, index) => {
             return <li key={'list' + index}>{item}</li>;
           })}
         </ul>
