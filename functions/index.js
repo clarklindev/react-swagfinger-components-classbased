be552321d19aeb229f8a55efa0021b12ecdad634
Clark <clarklindeveloper@gmail.com>
2020-01-28 01:05:42 +0200

    uploading via firebase functions - wrong directory (temp directory)

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,16 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
-const { Storage } = require('@google-cloud/storage');
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
 const os = require('os');
 const path = require('path');
 const spawn = require('child-process-promise').spawn;
+const fs = require('fs');
+//using google cloud storage
+//const { Storage } = require('@google-cloud/storage');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -12,57 +20,58 @@ const spawn = require('child-process-promise').spawn;
 // });
 
 //storage trigger .onFinalize() for create and rewrite existing
+//note: onChange is deprecated, using .onFinalize()
 exports.onFileChange = functions.storage
   .object()
-  .onFinalize((object, context) => {
-    console.log(object, context);
-    const bucket = object.bucket;
-    const contentType = object.contentType;
+  .onFinalize(async (object, context) => {
+    const fileBucket = object.bucket;
     const filePath = object.name;
-    console.log('File change detected, function execution started');
+    const contentType = object.contentType;
 
-    //prevent infinite loop
-    if (path.basename(filePath).startsWith('resized')) {
+    if (!contentType.startsWith('image/')) {
+      return console.log('This is not an image.');
+    }
+
+    //get the file name.
+    const fileName = path.basename(filePath);
+
+    //exit if image is already resized, prevent infinite loop
+    if (fileName.startsWith('renamed-')) {
       console.log('Already resized that file');
       return;
     }
 
-    //create a client
-    const storage = new Storage();
-
-    const destBucket = storage.bucket(bucket);
+    // Download file from bucket.
+    console.log('File change detected, function execution started');
+    const bucket = admin.storage().bucket(fileBucket);
     //temporary download to storage... gets cleaned up when function execution completes
-    const tmpFilePath = path.join(os.tmpdir(), path.basename(filePath));
-    const metadata = { contentType: contentType };
+    const tempFilePath = path.join(os.tmpdir(), fileName);
+    const metadata = {
+      contentType: contentType
+    };
 
-    //to download we need to pass destination where to download
-    return (
-      destBucket
-        .file(filePath)
-        .download({
-          destination: tmpFilePath
-        })
+    await bucket.file(filePath).download({ destination: tempFilePath });
+    console.log('Image downloaded locally to: ', tempFilePath);
 
-        // resize
-        .then(() => {
-          //spawn
-          //keeps aspect ratio, tries fitting existing image into this aspect ratio
-          return spawn('convert', [
-            tmpFilePath,
-            '-resize',
-            '500x500',
-            tmpFilePath
-          ]);
-        })
+    // Generate a thumbnail using ImageMagick.
+    await spawn('convert', [tempFilePath, '-resize', '500x500', tempFilePath]);
+    console.log('Image resized to', tempFilePath);
 
-        .then(() => {
-          //rename file
-          return destBucket.upload(tmpFilePath, {
-            destination: 'resized-' + path.basename(filePath),
-            metadata: metadata
-          });
-        })
+    // We add a 'renamed-' prefix to file name. That's where we'll upload the renamed file.
+    const renamedFileName = `renamed-${fileName}`;
+    const renamedFilePath = path.join(
+      path.dirname(tempFilePath),
+      renamedFileName
     );
+
+    //upload the resized/renamed file
+    await bucket.upload(tempFilePath, {
+      destination: renamedFilePath,
+      metadata: metadata
+    });
+
+    // Once the file has been uploaded, delete the local file to free up disk space.
+    return fs.unlinkSync(tempFilePath);
   });
 
 exports.onFileDelete = functions.storage
