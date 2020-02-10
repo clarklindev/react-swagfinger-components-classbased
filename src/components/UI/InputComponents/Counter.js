4c315aa7cb70e6e7659d0208e932d271886a9bd8
Clark <clarklindeveloper@gmail.com>
2020-02-10 11:29:03 +0200

    Counter Component Error Handling

--- src/components/UI/InputComponents/Counter.js
+++ src/components/UI/InputComponents/Counter.js
@@ -3,6 +3,8 @@ import classes from './Counter.module.scss';
 import InputContext from '../../../context/InputContext';
 import Icon from '../InputComponents/Icon';
 import Button from '../Button/Button';
+import ErrorList from './ErrorList';
+
 class Counter extends PureComponent {
   static contextType = InputContext;
 
@@ -43,6 +45,9 @@ class Counter extends PureComponent {
     if (event.target.value >= this.props.elementconfig.max) {
       this.context.changed(this.props.elementconfig.max, this.props.name);
     }
+    if (event.target.value === '') {
+      this.context.changed('', this.props.name);
+    }
   };
 
   onChangeHandler = (event) => {
@@ -62,22 +67,42 @@ class Counter extends PureComponent {
         ? true
         : false;
     let actual = this.props.value;
+    let tempClasses = [];
+    let error = null;
+    if (
+      this.props.validation &&
+      !this.props.value.valid &&
+      (this.props.value.touched ||
+        (!this.props.value.touched && !this.props.value.pristine))
+    ) {
+      console.log('pushing invalid: ');
+      tempClasses.push(classes.Invalid);
+      error = <ErrorList value={{ data: this.props.value.errors }} />;
+    }
     return (
       <div className={classes.Counter}>
-        <Button onClick={this.decrement} disabled={isMinBound}>
-          <Icon iconstyle='fas' code='minus' size='sm' />
-        </Button>
-        <input
-          step={this.props.elementconfig.increment}
-          ref={this.counterRef}
-          value={this.props.value.data}
-          onChange={(event) => this.onChangeHandler(event)}
-          onInput={(event) => this.onChangeHandler(event)}
-          onBlur={(event) => this.onBlur(event)}
-        />
-        <Button onClick={this.increment} disabled={isMaxBound}>
-          <Icon iconstyle='fas' code='plus' size='sm' />
-        </Button>
+        <div
+          className={[
+            classes.FlexGroupRow,
+            classes.Wrapper,
+            ...tempClasses
+          ].join(' ')}>
+          <Button onClick={this.decrement} disabled={isMinBound}>
+            <Icon iconstyle='fas' code='minus' size='sm' />
+          </Button>
+          <input
+            step={this.props.elementconfig.increment}
+            ref={this.counterRef}
+            value={this.props.value.data}
+            onChange={(event) => this.onChangeHandler(event)}
+            onInput={(event) => this.onChangeHandler(event)}
+            onBlur={(event) => this.onBlur(event)}
+          />
+          <Button onClick={this.increment} disabled={isMaxBound}>
+            <Icon iconstyle='fas' code='plus' size='sm' />
+          </Button>
+        </div>
+        {error}
       </div>
     );
   }
