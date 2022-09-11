13b2e1baae776eb9f27ef9773b8a0dc911db961d
swagfinger <swagfinger@gmail.com>
2022-09-11 11:40:37 +0800

    Checkbox changed to stateless component

--- src/components/UI/InputComponents/Checkbox.js
+++ src/components/UI/InputComponents/Checkbox.js
@@ -1,71 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classes from './Checkbox.module.scss';
 
-class Checkbox extends Component {
-  constructor(props) {
-    super(props);
+const Checkbox = (props)=> {
+  return (
+    <div className={[classes.Checkbox, props.className].join(' ')}>
+      <label className={classes.Container}>
+        <input
+          type="checkbox"
+          defaultChecked={props.checked}
+          name={props.name}
+          onChange={event => {
+            props.onChange(event.target.checked);
+          }}
+          disabled={props.isDisabled}
+        />
 
-    this.checkboxRef = React.createRef();
-  }
+        {props.indeterminate === true ? (
+          <span className={[classes.Indeterminate].join(' ')}></span>
+        ) : (
+          <span className={[classes.Checkmark].join(' ')}></span>
+        )}
 
-  state = {
-    checked: this.props.checked,
-    isDisabled: this.props.isDisabled
-  };
+        {props.label}
+      </label>
+    </div>
+  );
 
-  componentDidMount() {
-    this.setState({ checked: this.props.checked, isDisabled: this.props.isDisabled });
-  }
-
-  componentDidUpdate() {
-    if (this.state.checked !== this.props.checked) {
-      //console.log('COMPONENT DID UPDATE');
-      this.setState({ checked: this.props.checked });
-      this.checkboxRef.current.checked = this.props.checked;
-    }
-
-    if(this.state.isDisabled !== this.props.isDisabled){
-      this.setState({isDisabled :this.props.isDisabled});
-    }
-
-    if (this.props.indeterminate === true) {
-      this.checkboxRef.current.indeterminate = true;
-    } else {
-      this.checkboxRef.current.indeterminate = null;
-    }
-  }
-
-  render() {
-    return (
-      <div className={[classes.Checkbox, this.props.className].join(' ')}>
-        <label className={classes.Container}>
-          <input
-            type="checkbox"
-            defaultChecked={this.state.checked}
-            name={this.props.name}
-            ref={this.checkboxRef}
-            onChange={event => {
-              this.props.onChange(this.props.index, event.target.checked);
-            }}
-            disabled={this.state.isDisabled}
-          />
-          {/* {this.checkboxRef.current.indeterminate ? (
-            <span className={[classes.Intermediate].join(' ')}></span>
-          ) : (
-            <span className={[classes.Checkmark].join(' ')}></span>
-          )} */}
-
-          {this.props.indeterminate === true ? (
-            <span className={[classes.Indeterminate].join(' ')}></span>
-          ) : (
-            <span className={[classes.Checkmark].join(' ')}></span>
-          )}
-
-          {this.props.label}
-        </label>
-      </div>
-    );
-  }
 }
 
 export default Checkbox;
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,16 +1,59 @@
 import DefaultPageLayout from '../../hoc/DefaultPageLayout/DefaultPageLayout';
-import Card from '../../components/UI/Card/Card';
 
-const Home = () => {
-  return (
-    <div>
-      <DefaultPageLayout label='Components'>
-        <Card style={['Padding']}>
-          
-        </Card>
-      </DefaultPageLayout>
-    </div>
-  );
+import Card from '../../components/UI/Card/Card';
+import {Accordion, Breadcrumb, Checkbox, CheckboxCollection, Counter, Datepicker, DraggableItem, ErrorList, Expandable, GalleryItem, GalleryManager, Input, InputPassword, InputSearch, InputWithIcon, InputWithInput, List, ListItem, MultiInput, MultiInputObjects, MultiRangeSlider, MultiSelect, MultiSelectWithInput, RadioButton, RadioCollection, RangeSlider, Select, SelectToAccordion, Text, TextArea, Toggle}  from '../../components/UI/InputComponents';
+import React, { Component } from 'react';
+class Home extends Component {
+
+  state = {
+    checkboxChecked: false
+  }
+
+  accordionConfig = {
+    allowMultiOpen: false,
+    openOnStartIndex: -1, //zero-index, negative value or invalid index to not open on start,
+    hovereffect: true,
+    onClick: () => {
+      console.log('ouside onclick');
+    },
+    style: {},
+  };
+
+  checkboxConfig = {
+    checked : this.state.checkboxChecked,
+    onChange : (value)=>{
+      this.setState({checkboxChecked:value});
+    }
+  };
+
+  render(){
+    return (
+      <div>
+        <DefaultPageLayout label='Components'>
+          <Card style={['Padding']}>
+              <h4>Accordion</h4>
+
+              {/* accordion content must be passed in as children of Accordion element */}
+              <Accordion {...this.accordionConfig}>
+                <div label={<strong>question1</strong>}>content1</div>
+                <div label={<strong>question2</strong>}>content2</div>
+                <div label={<strong>question3</strong>}>content3</div>
+              </Accordion>
+              <br/>
+              {/* specific to firebase <Breadcrumb></Breadcrumb> */}
+  
+                
+              {/* basic checkbox */}
+              <Checkbox {...this.checkboxConfig}/>
+
+              {/* multicheckbox */}
+              
+          </Card>
+        </DefaultPageLayout>
+      </div>
+    );
+  }
+  
 
 };
 
